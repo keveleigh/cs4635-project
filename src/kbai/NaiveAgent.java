1a20a8235f78d99119e5d0eb31c59382e0d092b4
keveleigh <kurtis.eveleigh@gmail.com>
2013-09-23 19:30:56 -0400

    Lol the agents

---
+++ src/kbai/NaiveAgent.java
@@ -0,0 +1,31 @@
+package kbai;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class NaiveAgent extends Agent{
+	
+	private Random rand;
+	
+	public NaiveAgent()
+	{
+		rand = new Random();
+	}
+	
+	public int nextMove(int[][] board)
+	{
+		ArrayList<Integer> open = new ArrayList<Integer>();
+		for(int j=0; j<board.length; j++)
+		{
+			for(int i=0; i<board[0].length; i++)
+			{
+				if(board[j][i] == 0)
+				{
+					open.add(j*3 + i);
+				}
+			}
+		}
+		int move = rand.nextInt(open.size());
+		return open.get(move);
+	}
+}
---
+++ src/kbai/ThoughtfulAgent.java
@@ -0,0 +1,10 @@
+package kbai;
+
+public class ThoughtfulAgent extends Agent{
+
+	public int nextMove(int[][] board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
