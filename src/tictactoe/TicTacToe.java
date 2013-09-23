f3334bb0095248490807c750ee071b58f256f6cc
keveleigh <kurtis.eveleigh@gmail.com>
2013-09-23 14:33:06 -0400

    Initial commit.
    Includes all reusable files from Project 1 and some base git files.

---
+++ src/tictactoe/TicTacToe.java
@@ -0,0 +1,147 @@
+package tictactoe;
+
+import kbai.Agent;
+
+public class TicTacToe {
+
+	private int[][] board;
+	private Agent p1;
+	private Agent p2;
+	private String[] symbol;
+	
+	public TicTacToe(Agent p1, Agent p2)
+	{
+		board = new int[3][3];
+		this.p1 = p1;
+		this.p2 = p2;
+		symbol = new String[3];
+		symbol[0] = " ";
+		symbol[1] = "X";
+		symbol[2] = "O";
+	}
+	
+	public void takeMove(int player)
+	{
+		if(player == 1)
+		{
+			int move = p1.nextMove(board);
+			board[move/3][move%3] = 1;
+		}
+		else if(player == 2)
+		{
+			int move = p2.nextMove(board);
+			board[move/3][move%3] = 2;
+		}
+	}
+	
+	public String printBoard()
+	{
+		String ret = "";
+		for(int j=0; j<board.length; j++)
+		{
+			for(int i=0; i<board[0].length; i++)
+			{
+				ret += " ";
+				ret += symbol[board[j][i]];
+				if(i < 2){
+					ret += " |";
+				}
+			}
+			if(j < 2){
+				ret += "\n-----------\n";
+			}
+			else
+			{
+				ret += "\n";
+			}
+		}		
+		return ret;
+	}
+	
+	public boolean noWinner()
+	{
+		if(board[0][0] != 0 && board[0][0] == board[0][1] && board[0][1] == board[0][2])
+		{
+			return false;
+		}
+		else if(board[1][0] != 0 && board[1][0] == board[1][1] && board[1][1] == board[1][2])
+		{
+			return false;
+		}
+		else if(board[2][0] != 0 && board[2][0] == board[2][1] && board[2][1] == board[2][2])
+		{
+			return false;
+		}
+		else if(board[0][0] != 0 && board[0][0] == board[1][0] && board[1][0] == board[2][0])
+		{
+			return false;
+		}
+		else if(board[0][1] != 0 && board[0][1] == board[1][1] && board[1][1] == board[2][1])
+		{
+			return false;
+		}
+		else if(board[0][2] != 0 && board[0][2] == board[1][2] && board[1][2] == board[2][2])
+		{
+			return false;
+		}
+		else if(board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+		{
+			return false;
+		}
+		else if(board[0][2] != 0 && board[0][2] == board[1][1] && board[1][1] == board[2][0])
+		{
+			return false;
+		}
+		else
+		{
+			for(int j=0; j<board.length; j++)
+			{
+				for(int i=0; i<board[0].length; i++)
+				{
+					if(board[j][i] == 0)
+					{
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	public String getWinner()
+	{
+		if(board[0][0] != 0 && board[0][0] == board[0][1] && board[0][1] == board[0][2])
+		{
+			return symbol[board[0][0]];
+		}
+		else if(board[1][0] != 0 && board[1][0] == board[1][1] && board[1][1] == board[1][2])
+		{
+			return symbol[board[1][0]];
+		}
+		else if(board[2][0] != 0 && board[2][0] == board[2][1] && board[2][1] == board[2][2])
+		{
+			return symbol[board[2][0]];
+		}
+		else if(board[0][0] != 0 && board[0][0] == board[1][0] && board[1][0] == board[2][0])
+		{
+			return symbol[board[0][0]];
+		}
+		else if(board[0][1] != 0 && board[0][1] == board[1][1] && board[1][1] == board[2][1])
+		{
+			return symbol[board[0][1]];
+		}
+		else if(board[0][2] != 0 && board[0][2] == board[1][2] && board[1][2] == board[2][2])
+		{
+			return symbol[board[0][2]];
+		}
+		else if(board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+		{
+			return symbol[board[0][0]];
+		}
+		else if(board[0][2] != 0 && board[0][2] == board[1][1] && board[1][1] == board[2][0])
+		{
+			return symbol[board[0][2]];
+		}
+		return null;		
+	}
+}
